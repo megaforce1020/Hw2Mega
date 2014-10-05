328ee4a147db76c52620791f140e93e7fbd996bf
Kevin <Kevin@Kevin-PC>
2014-10-04 20:40:26 -0600

    homework commit

---
+++ src/hw2kevincai/Hw2KevinCai.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw2kevincai;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Hw2KevinCai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
